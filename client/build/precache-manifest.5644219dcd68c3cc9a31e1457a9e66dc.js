fdfbfd1c8f81e5d29f6e4f79607f883d70f5383c
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-09-13 14:35:11 -0400

    Updated .gitignore file

---
+++ client/build/precache-manifest.5644219dcd68c3cc9a31e1457a9e66dc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "747c77e92107f09ad905",
+    "url": "/static/js/main.9a96db0d.chunk.js"
+  },
+  {
+    "revision": "d3097745a81be6eee543",
+    "url": "/static/js/2.e56ebcfa.chunk.js"
+  },
+  {
+    "revision": "2bc3008d9fc961e208664a55c1421732",
+    "url": "/index.html"
+  }
+];
